1ec2ce94e9b0d596a5a47f2925f18c103d208d32
Semyon Yanichev <yas@d-element.ru>
2022-07-27 19:53:59 +0400

    filter pattern

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\BaseController;
+use App\Http\Filters\PostFilter;
 use App\Http\Requests\Post\FilterRequest;
 use App\Models\Post;
 
@@ -10,18 +11,11 @@ class IndexController extends BaseController
 {
     public function __invoke(FilterRequest $request)
     {
-//        $posts = Post::paginate(10);
-//        return view('posts.index', compact('posts'));
         $data = $request->validated();
-        $query = Post::query();
-        if(isset($data['category_id'])) {
-            $query->where('category_id', $data['category_id']);
-        }
-        if(isset($data['title'])) {
-            $query->where('title', 'like', "%{$data['title']}%");
-        }
-        $posts = $query->get();
-        dd($posts);
+        $filter = app()->make(PostFilter::class, ['queryParams' => $data]);
+        $posts = Post::filter($filter)->paginate(10)->withQueryString();
+
+        return view('posts.index', compact('posts'));
 
     }
 }
