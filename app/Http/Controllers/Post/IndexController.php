b9f7b77c158628be56900d678893baa9c0b25663
Semyon Yanichev <yas@d-element.ru>
2022-07-27 16:54:24 +0400

    filter test

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\BaseController;
+use App\Http\Requests\Post\FilterRequest;
 use App\Models\Post;
 
 class IndexController extends BaseController
 {
-    public function __invoke()
+    public function __invoke(FilterRequest $request)
     {
-        $posts = Post::paginate(10);
-        return view('posts.index', compact('posts'));
+//        $posts = Post::paginate(10);
+//        return view('posts.index', compact('posts'));
+        $data = $request->validated();
+        $query = Post::query();
+        if(isset($data['category_id'])) {
+            $query->where('category_id', $data['category_id']);
+        }
+        if(isset($data['title'])) {
+            $query->where('title', 'like', "%{$data['title']}%");
+        }
+        $posts = $query->get();
+        dd($posts);
+
     }
 }
