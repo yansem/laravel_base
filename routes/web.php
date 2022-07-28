23b1d4a887208b5e7377003ce4269eb83e605f12
Semyon Yanichev <yas@d-element.ru>
2022-07-28 12:28:03 +0400

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function() {
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => 'admin'], function() {
     Route::group(['namespace' => 'Post'], function() {
         Route::get('/posts', 'IndexController')->name('admin.post.index');
         Route::get('/posts/create', 'CreateController')->name('admin.post.create');
